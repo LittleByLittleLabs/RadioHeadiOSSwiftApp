8074afb4b176043f070a0316007a60ed75699531
Armen Donigian <donigian@gmail.com>
2014-11-30 11:33:56 -0800

    Added a movie quote along with button to PlaySoundsViewController.

---
+++ Radio Head/PlaySoundsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PlaySoundsViewController.swift
+//  Radio Head
+//
+//  Created by arm on 11/30/14.
+//  Copyright (c) 2014 arm. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+
+    var audioPlayer : AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+        } else {
+            println("filePaht is empty")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSlowAudio(sender: UIButton) {
+        audioPlayer.play()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
