04d4fdf217d65fc27823364d8914ce3a4bfbc9e7
Armen Donigian <donigian@gmail.com>
2014-11-30 11:38:49 -0800

    Added a movie quote along with button to PlaySoundsViewController.

--- Radio Head/PlaySoundsViewController.swift
+++ Radio Head/PlaySoundsViewController.swift
@@ -20,6 +20,7 @@ class PlaySoundsViewController: UIViewController {
         if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
             var filePathUrl = NSURL.fileURLWithPath(filePath)
             audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+            audioPlayer.enableRate = true
         } else {
             println("filePaht is empty")
         }
@@ -31,6 +32,7 @@ class PlaySoundsViewController: UIViewController {
     }
     
     @IBAction func playSlowAudio(sender: UIButton) {
+        audioPlayer.rate = 0.5
         audioPlayer.play()
     }
 
