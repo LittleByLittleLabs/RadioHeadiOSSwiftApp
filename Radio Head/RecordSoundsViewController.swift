c193cc1f19a1d18e2e9f2093cc3f3c128d914fa8
Armen Donigian <donigian@gmail.com>
2014-11-30 16:04:49 -0800

    Added Audio Recording feature to app.

--- Radio Head/RecordSoundsViewController.swift
+++ Radio Head/RecordSoundsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
     @IBOutlet weak var RecordingInProgress: UILabel!
@@ -15,8 +16,13 @@ class RecordSoundsViewController: UIViewController {
     
     @IBOutlet weak var recordButton: UIButton!
     
+    var audioRecorder: AVAudioRecorder!
+    
     @IBAction func stopAudio(sender: UIButton) {
         RecordingInProgress.hidden = true
+        audioRecorder.stop()
+        var audioSession = AVAudioSession.sharedInstance()
+        audioSession.setActive(false, error: nil)
     }
 
 
@@ -38,7 +44,25 @@ class RecordSoundsViewController: UIViewController {
         RecordingInProgress.hidden = false
         stopButton.hidden = false
         recordButton.enabled = false
-        println("in record")
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        let currentDateTime = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "ddMMyyyy-HHmmss"
+        let recordingName = formatter.stringFromDate(currentDateTime)+".wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        println(filePath)
+        
+        var session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
+        
+        audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+
     }
 
 }
